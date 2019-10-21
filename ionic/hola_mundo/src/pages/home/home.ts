373a9d2d43a70cbc1fb5a1d0c875d3e36695acbc
Adrian <adrianarboledadavis@gmail.com>
2019-10-21 12:21:31 -0500

    alerta de AlertController

--- ionic/hola_mundo/src/pages/home/home.ts
+++ ionic/hola_mundo/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,11 +9,19 @@ export class HomePage {
 
   nombre='';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public alertCtrl: AlertController) {
 
   }
   click(){
-    console.log('Hola', this.nombre);
+    //console.log('Hola', this.nombre);
+  const alerta = this.alertCtrl.create({
+    title: 'Hola',
+    subTitle: this.nombre,
+    buttons: ['Ok', 'Cancel']
+  });
+
+  alerta.present();
   }
 
 }
